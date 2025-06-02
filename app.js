59c5c381223924cc60d556d0d77fb37a16dda2de
ari-me <1arwenuy@gmail.com>
2025-06-02 04:11:59 +0400

    render commit

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const suggestions = [
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
